7210e36e1266074d609708b9171fd5b0b3e8ce58
张建奋 <zhangjianfen@outlook.com>
2016-05-01 12:49:40 +0800

    [Added] svg dynamic .

--- src/main/java/com/dxw/flfs/ui/SvgPanel.java
+++ src/main/java/com/dxw/flfs/ui/SvgPanel.java
@@ -1,5 +1,9 @@
 package com.dxw.flfs.ui;
 
+import com.dxw.flfs.communication.PlcDelegate;
+import com.dxw.flfs.communication.PlcDelegateFactory;
+import com.dxw.flfs.communication.PlcModel;
+import com.dxw.flfs.communication.PlcModelField;
 import org.apache.batik.swing.JSVGCanvas;
 import org.apache.batik.swing.gvt.GVTTreeRendererAdapter;
 import org.apache.batik.swing.gvt.GVTTreeRendererEvent;
@@ -7,6 +11,8 @@ import org.apache.batik.swing.svg.GVTTreeBuilderAdapter;
 import org.apache.batik.swing.svg.GVTTreeBuilderEvent;
 import org.apache.batik.swing.svg.SVGDocumentLoaderAdapter;
 import org.apache.batik.swing.svg.SVGDocumentLoaderEvent;
+import org.w3c.dom.Element;
+import org.w3c.dom.svg.SVGDocument;
 
 import javax.swing.*;
 import java.awt.*;
@@ -20,14 +26,90 @@ public class SvgPanel {
     private JPanel panel;
 
     private JSVGCanvas canvas;
+    public SvgPanel(){
+        initSvg();
+
+        PlcDelegate delegate = PlcDelegateFactory.getPlcDelegate();
+        delegate.addModelChangedListener(event -> {
+            long field = event.getField();
+            PlcModel model = event.getModel();
+
+            SVGDocument document = canvas.getSVGDocument();
+            if (field == PlcModelField.SYSTEM_STATUS) {
+                Short status = model.getSystemStatus();
+                Element ele = document.getElementById("textSystemStatus");
+
+                switch (status) {
+                    case 1:
+                        ele.setTextContent("停机");
+                        break;
+                    case 2:
+                        ele.setTextContent("做料");
+                        break;
+                    case 3:
+                        ele.setTextContent("清洗");
+                        break;
+                    case 4:
+                        ele.setTextContent("紧停");
+                        break;
+                    case 5:
+                        ele.setTextContent("冷启动");
+                        break;
+                }
+
+            } else if (field == PlcModelField.FERMENT_BARREL_STATUS) {
+                boolean[] data = model.getFermentBarrelStatus();
+                String status = "";
+                for (int i = 0; i < Math.min(data.length,7); i++) {
+                    status += ((data[i]) ? "满" : "空") + " ";
+                }
+                Element ele = document.getElementById("textFermentBarrelStatus");
+                ele.setTextContent(status);
+
+            } /*else if (field == PlcModelField.MATERIAL_TOWER_ALARM) {
+                Boolean lowAlarm = model.getMaterialTowerLowAlarm();
+                Boolean emptyAlarm = model.getMaterialTowerEmptyAlarm();
+
+                if (lowAlarm) {
+                    lblMaterialTowerLow.setIcon(iconAlert);
+                    lblMaterialTowerLow.setText("");
+                } else {
+                    lblMaterialTowerLow.setIcon(null);
+                    lblMaterialTowerLow.setText("正常");
+                }
+
+                if (emptyAlarm) {
+                    lblMaterialTowerEmpty.setIcon(iconAlert);
+                    lblMaterialTowerEmpty.setText("");
+                } else {
+                    lblMaterialTowerEmpty.setIcon(null);
+                    lblMaterialTowerEmpty.setText("正常");
+                }
+            } else if (field == PlcModelField.FERMENT_BARREL_IN_OUT) {
+                short in = model.getFermentBarrelInNo();
+                short out = model.getFermentBarrelOutNo();
+                lblFermentBarrelIn.setText(Short.toString(in));
+                lblFermentBarrelOut.setText(Short.toString(out));
+            } else if (field == PlcModelField.MIXING_BARREL_STATUS) {
+                Short status = model.getMixingBarrelStatus();
+                lblMixingBarrelStatus.setText(status == 0 ? "空闲" : "运行");
+            } else if (field == PlcModelField.PH_VALUE) {
+                float ph = model.getPh();
+                lblPh.setText(Float.toString(ph));
+            }*/
+            //canvas.repaint();
+            //canvas.getCanvasGraphicsNode().fireGraphicsNodeChangeCompleted();
+            canvas.getUpdateManager().forceRepaint();
+        });
+    }
 
     private void createUIComponents() {
-
         panel = new JPanel();
         panel.setBackground(new Color(255, 255, 255));
         panel.setLayout(new BorderLayout());
-        initSvg();
     }
+
+
 
     private void initSvg() {
         canvas = new JSVGCanvas();
